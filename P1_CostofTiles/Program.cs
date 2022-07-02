1a9bdb76bf12d56a5ec6e1dfef17e9043b1d3d9a
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-02 15:19:15 -0400

    Input data from users Convert data from String to Int

--- P1_CostofTiles/Program.cs
+++ P1_CostofTiles/Program.cs
@@ -6,7 +6,25 @@ namespace P1_CostofTiles // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double length;
+            double width;
+            double costPerUnitOfTile;
+            double totalCost;
+
+            Console.WriteLine("Find the Cost of Tile to Cover W x L Floor");
+            
+            Console.Write("Enter the length in square meters: ");
+            length = Int32.Parse(Console.ReadLine());
+            
+            Console.Write("Enter the width in square meters: ");
+            width = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter the unit cost per tile in square meters: ");
+            costPerUnitOfTile = Int32.Parse(Console.ReadLine());
+
+            totalCost = (length * width) * costPerUnitOfTile;
+
+            Console.WriteLine($"The total cost is ${totalCost}");
         }
     }
 }
