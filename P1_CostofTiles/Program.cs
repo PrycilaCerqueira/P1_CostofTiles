e8a3b60da67edd511b7319f0451aada32d7882a0
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-02 15:36:15 -0400

    Double input convert Calculation of Tiles

--- P1_CostofTiles/Program.cs
+++ P1_CostofTiles/Program.cs
@@ -6,22 +6,21 @@ namespace P1_CostofTiles // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            int length;
-            int width;
-            int costPerUnitOfTile;
-            int totalCost;
+            double length;
+            double width;
+            double costPerUnitOfTile;
+            double totalCost;
 
             Console.WriteLine("Find the Cost of Tile to Cover W x L Floor");
             
             Console.Write("Enter the length in square meters: ");
-            length = Int32.Parse(Console.ReadLine());
-            Convert.ToDouble(length);
-            
+            length = Double.Parse(Console.ReadLine());
+                       
             Console.Write("Enter the width in square meters: ");
-            width = Int32.Parse(Console.ReadLine());
+            width = Double.Parse(Console.ReadLine());
 
             Console.Write("Enter the unit cost per tile in square meters: ");
-            costPerUnitOfTile = Int32.Parse(Console.ReadLine());
+            costPerUnitOfTile = Double.Parse(Console.ReadLine());
 
             totalCost = (length * width) * costPerUnitOfTile;
 
