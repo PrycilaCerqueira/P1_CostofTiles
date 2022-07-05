b311274f741e4bc3e2c00bb2951d22709f221448
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-05 13:17:48 -0400

    Implement the menu with switch

--- P1_CostofTiles/Program.cs
+++ P1_CostofTiles/Program.cs
@@ -10,6 +10,7 @@ namespace P1_CostofTiles // Note: actual namespace depends on the project name.
             double length;
             double width;
             double height;
+            double sum;
             double area;
             double costPerUnitOfTile;
             double totalCost;
@@ -26,25 +27,35 @@ namespace P1_CostofTiles // Note: actual namespace depends on the project name.
             Console.Write(" Enter the unit cost per tile in meters: ");
             costPerUnitOfTile = Double.Parse(Console.ReadLine());
 
-            Console.Write("\n What is the area shape you would like to calculate? \n 1) Triangle \n 2) Square \n 3) Rectangle \n Enter your option number here: ");
+            Console.Write("\n What is the area shape you would like to calculate? \n 1) Triangle \n 2) Trapezoid \n 3) Rectangle \n Enter your option number here: ");
             menuOption = Int32.Parse(Console.ReadLine());
             
             switch (menuOption)
             { 
                 case 1: //Triangle
-                    area = (length * height) / 2;
+                    sum = (length + width + height) / 2;
+                    area = Math.Sqrt(sum * (sum - length) * (sum - width) * (sum - height));
                     totalCost = area * costPerUnitOfTile;
-                    Console.WriteLine($"\n The total cost for covering a Tringle area of {area}m is ${totalCost}");
+                    Console.WriteLine($"\n The total cost for covering a Tringle area of {area} square meters is ${totalCost}.");
                     break;
 
+                case 2: //Trapezoid
+                    area = ((length + width) / 2) * height;
+                    totalCost = area * costPerUnitOfTile;
+                    Console.WriteLine($"\n The total cost for covering a Trapezoid area of {area} square meters is ${totalCost}.");
+                    break;
 
-            
+                case 3: //Rectangle
+                    area = length * width;
+                    totalCost = area * costPerUnitOfTile;
+                    Console.WriteLine($"\n The total cost for covering a Rectangle area of {area} square meters is ${totalCost}.");
+                    break;
+                   
+                default:
+                    Console.WriteLine("\n Invalid option. \n Try again!");
+                    break;
             }
 
-
-            //totalCost = (length * width) * costPerUnitOfTile;
-
-            //Console.WriteLine($"The total cost is ${totalCost}");
         }
     }
 }
