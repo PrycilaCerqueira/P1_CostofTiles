11d78d9544a253a11a83f842e234cccb73127398
Pry Cerqueira <75625742+PrycilaCerqueira@users.noreply.github.com>
2022-07-05 12:41:57 -0400

    Added the menu with switch

--- P1_CostofTiles/Program.cs
+++ P1_CostofTiles/Program.cs
@@ -6,25 +6,45 @@ namespace P1_CostofTiles // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            int menuOption;
             double length;
             double width;
+            double height;
+            double area;
             double costPerUnitOfTile;
             double totalCost;
 
-            Console.WriteLine("Find the Cost of Tile to Cover W x L Floor");
-            
-            Console.Write("Enter the length in square meters: ");
+            Console.Write("Find the Cost of Tile to Cover the Floor \n\n Enter the length in meters: ");
             length = Double.Parse(Console.ReadLine());
-                       
-            Console.Write("Enter the width in square meters: ");
+
+            Console.Write(" Enter the width in meters: ");
             width = Double.Parse(Console.ReadLine());
 
-            Console.Write("Enter the unit cost per tile in square meters: ");
+            Console.Write(" Enter the height in meters: ");
+            height = Double.Parse(Console.ReadLine());
+
+            Console.Write(" Enter the unit cost per tile in meters: ");
             costPerUnitOfTile = Double.Parse(Console.ReadLine());
 
-            totalCost = (length * width) * costPerUnitOfTile;
+            Console.Write("\n What is the area shape you would like to calculate? \n 1) Triangle \n 2) Square \n 3) Rectangle \n Enter your option number here: ");
+            menuOption = Int32.Parse(Console.ReadLine());
+            
+            switch (menuOption)
+            { 
+                case 1: //Triangle
+                    area = (length * height) / 2;
+                    totalCost = area * costPerUnitOfTile;
+                    Console.WriteLine($"\n The total cost for covering a Tringle area of {area}m is ${totalCost}");
+                    break;
 
-            Console.WriteLine($"The total cost is ${totalCost}");
+
+            
+            }
+
+
+            //totalCost = (length * width) * costPerUnitOfTile;
+
+            //Console.WriteLine($"The total cost is ${totalCost}");
         }
     }
 }
